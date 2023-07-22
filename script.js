fda22557b15de8b9e1e88d136afc95c896141e63
itsy bitsy <119555750+ltsyBitsy@users.noreply.github.com>
2023-07-21 21:15:17 -0400

    Create functions for math operations

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y !==  0) {
+        return x / y;
+    }
+    else return Error;
+}
